30783dc102fe72bc3ac3294eb76dd8dc5816f75b
shrink0r <schmittrink@gmail.com>
2019-02-20 17:40:41 +0100

    make sure to cast int values after passing assert

--- src/ValueObject/IntValue.php
+++ src/ValueObject/IntValue.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the daikon-cqrs/entity project.
  *
@@ -6,7 +7,7 @@
  * file that was distributed with this source code.
  */
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace Daikon\Entity\ValueObject;
 
@@ -19,28 +20,28 @@ final class IntValue implements ValueObjectInterface
     private $value;
 
     /** @param int|null $value  */
-    public static function fromNative($value): IntValue
+    public static function fromNative($value) : IntValue
     {
         Assertion::nullOrIntegerish($value, 'Trying to create IntValue VO from unsupported value type.');
-        return new self($value);
+        return new self((int)$value);
     }
 
-    public function toNative(): ?int
+    public function toNative() : ? int
     {
         return $this->value;
     }
 
-    public function equals(ValueObjectInterface $value): bool
+    public function equals(ValueObjectInterface $value) : bool
     {
         return $value instanceof self && $this->toNative() === $value->toNative();
     }
 
-    public function __toString(): string
+    public function __toString() : string
     {
         return is_null($this->value) ? 'null' : (string)$this->value;
     }
 
-    private function __construct(?int $value)
+    private function __construct(? int $value)
     {
         $this->value = $value;
     }
